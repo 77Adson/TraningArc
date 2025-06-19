cc5b73e5640649497ed7b19738ecafbb586ec5c1
77Adson <11maczupaczu@gmail.com>
2025-06-20 01:16:04 +0200

    scrolling added for exercise

--- app/src/main/java/com/example/trainingarc/features/homePage/screens/ExerciseScreen.kt
+++ app/src/main/java/com/example/trainingarc/features/homePage/screens/ExerciseScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -77,10 +79,13 @@ fun ExerciseScreen(
             )
         }
     ) { innerPadding ->
+        val scrollState = rememberScrollState()
+
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(16.dp)
+                .verticalScroll(scrollState)
         ) {
             ExerciseDescriptionField(
                 description = localDescription,
